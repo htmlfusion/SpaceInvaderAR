8d87e01773c4e255597036edf803b2115c0fe618
Jesse Vander Does <jesse@htmlfusion.com>
2015-11-13 14:30:29 -0800

    Cause

---
+++ Assets/Scripts/OnImpact.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OnImpact : MonoBehaviour {
+	
+	public int scoreValue;
+	private GameController gameController;
+	
+	
+	void Start()
+	{
+		// Go and grab the gamecontroller object
+		GameObject gameControllerObject = GameObject.FindGameObjectWithTag("GameController");
+		if (gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent<GameController>();
+		}
+		if (gameController == null)
+		{
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+	
+	void Update() {
+	}
+	
+	void OnCollisionEnter(Collision other)
+	{
+
+		GetComponent<AudioSource> ().Play ();
+
+		Debug.Log ("Collision blue");
+		gameController.AddScore(scoreValue);
+		//Destroy(other.gameObject);
+		//Destroy(gameObject);
+	}
+}
